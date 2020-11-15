5a9a67074df241d177b39721de8778846df8b867
Philo <philolegall@hotmail.fr>
2020-11-15 16:06:24 +0100

    adaptation of function to real dataset (with missing data)

--- regional_inference_functions.R
+++ regional_inference_functions.R
@@ -5,7 +5,7 @@ library(evd)#qgpd
 ###############################################
 ######### FIT EGPD(ki,s,xi)
 
-fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(1,Inf),ParReg = "mean", thres=0){
+fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(0,Inf),ParReg = "mean", thres=0){
   #   DESCRIPTION
   # Fit a regional version of EGPD in Naveau et al. (2016).
   #   ARGUMENTS
@@ -28,23 +28,25 @@ fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(1,Inf),ParReg = 
     M = as.matrix(M,ncol=1)}
   nday = nrow(M)
   nstat = ncol(M)
-  MN = matrix(NA,nrow = nday,ncol = nstat)
+  
   as_mean = as_Lcensor_thres = rep(NA,nstat)
   as_Rcensor_thres = rep(Inf,nstat)
   #consider only days with data
   obs_rr = matrix(nrow=0,ncol=0)
+  obs_norm = matrix(0,nrow = 0,ncol =0)
   for (station in 1:nstat){
     y = M[,station]
     y = y[!is.na(y)]
     y = y[y>thres]-thres#consider only positive precipitation when thres =0
-    as_mean[station] = mean(y,na.rm=TRUE)
+    as_mean[station] = moy= mean(y,na.rm=TRUE)
     obs_rr = rbind.fill.matrix(obs_rr,t(as.matrix(y)))#and fill the matrix with NA if needed
+    obs_norm = rbind.fill.matrix(obs_norm,t(as.matrix(y/moy)))#and fill the matrix with NA if needed
   }
   M = t(obs_rr)
+  MN=t(obs_norm)
   fitreg = as.data.frame(matrix(NA,nrow = 4,ncol=nstat),
                          row.names = c("kappa","sigma","xi.site","xi.reg"))
   for (station in 1:nstat) {
-    moy = as_mean[station]
     y = na.omit(M[,station])
     as_Lcensor_thres[station] = cens_thres[1]/moy
     as_Rcensor_thres[station] = cens_thres[2]/moy
@@ -53,13 +55,13 @@ fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(1,Inf),ParReg = 
     fitreg[1:3,station] = fit.extgp(y[y>0],method = method,init=InitPar,
                                     censoring = cens_thres,
                                     plots = FALSE)$fit$pwm
-    MN[,station] = y/moy
   }
   yn = as.vector(MN)
-  Xi.reg.pool = fit.extgp(yn[yn>0],method=method,init = InitPar,
-                          censoring = c(max(as_Lcensor_thres),min(as_Rcensor_thres)),plots = FALSE)$fit$pwm[3]
+  InitPar = c(.9,.5,.1)
+  #Xi.reg.pool = fit.extgp(yn[yn>0],method=method,init = InitPar,
+  #censoring = c(max(as_Lcensor_thres),min(as_Rcensor_thres)),plots = FALSE)$fit$pwm[3]
   Xi.reg.mean = mean(data.matrix(fitreg[3,]))
-  if(ParReg=="pool"){xi.reg = Xi.reg.pool}
+  #if(ParReg=="pool"){xi.reg = Xi.reg.pool}
   if(ParReg=="mean"){xi.reg = Xi.reg.mean}
   Kappa.site = as.vector(data.matrix(fitreg[1,]))
   SigmaSemiReg = as_mean*xi.reg/(Kappa.site*beta(Kappa.site,1-xi.reg)-1)
@@ -67,7 +69,6 @@ fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(1,Inf),ParReg = 
   fitreg[4,] = rep(xi.reg,nstat)
   return(fitreg)
 }
-
 
 
 
