87da5f4179c5677ee78d3726dfab2ba2f84f3097
Philo <philolegall@hotmail.fr>
2020-11-26 12:21:54 +0100

    iterative and censored version of semi-regional fit

--- regional_inference_functions.R
+++ regional_inference_functions.R
@@ -1,13 +1,34 @@
-##################### FUNCTIONS FOR REGIONAL INFERENCE ########################
+######### FIT EGPD(k,s,xi)
+library(mev)
+library(evd)#pgpd
 library(plyr)#rbind.fill.matrix
-library(mev)#egp2.fit, qegd,...
-library(evd)#qgpd
-###############################################
-######### FIT EGPD(ki,s,xi)
+library(zipfR)#incomplete beta function
+############################
+## PRELIMINARY FUNCTIONS
+############################
+IB <- function(x,y,a,b){
+  #incomplete beta as defined in appendix of Naveau et al., 2016
+  z = Ibeta(y,a,b)-Ibeta(x,a,b)
+  return(z)
+}
+H <- function(x,sigma,xi){
+  z = pgpd(x,scale=sigma,shape=xi)
+  return(z)
+}
+pG = function(kappa,u){
+  fct=u^kappa# = G(u)
+  return(fct)
+}
+Fbar <- function(x,kappa,sigma,xi){
+  z = 1-pG(kappa,H(x,sigma,xi))
+  return(z)
+}
 
-fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(0,Inf),ParReg = "mean", thres=0){
+fitEGPDkSemiRegCensoredIter <- function(M,method="pwm",cens_thres=c(0,Inf),round=0.1,
+                                        ParReg = "mean", thres=0, precision=.0001,
+                                        ParInit=c(0.5,0.5,0.2), loop.max = 50){
   #   DESCRIPTION
-  # Fit a regional version of EGPD in Naveau et al. (2016).
+  # Fit a (semi)regional version of EGPD in Naveau et al. (2016).
   #   ARGUMENTS
   # M, matrix of positive precipitation 
   # Each row corresponds to a day, each col to a site
@@ -18,9 +39,10 @@ fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(0,Inf),ParReg = 
   # thres = threshold of wet days (e.g. 2mm) if not null, provides conditional parameters
   
   #   VALUE
-  # List of parameters: 
-  #   - the fist element of the list is the list of semi-regional scale parameters
-  #   - the second and last element of the list contains regional shape parameter kappa and the regional shape parameter xi estimated on pooled normalized data
+  # List of parameters (Theta,Theta_0)
+  #   - the fist element of the list is the list of semi-regional flexibility parameter kappa,
+  #     semi-regional scale parameters sigma and regional shape parameter xi.
+  #   - the second element of the list contains initialization of parameters (i.e. at-site estimates)
   ###############################################
   #if only one temporal serie put in a matrix
   
@@ -29,48 +51,76 @@ fitEGPDkSemiRegCensored <- function(M,method="pwm",cens_thres=c(0,Inf),ParReg = 
   nday = nrow(M)
   nstat = ncol(M)
   
-  as_mean = as_Lcensor_thres = rep(NA,nstat)
-  as_Rcensor_thres = rep(Inf,nstat)
+  Sigma_names=Kappa_names=rep(NA,nstat)
+  CensoredMean = moyN  = rep(NA,nstat)
   #consider only days with data
   obs_rr = matrix(nrow=0,ncol=0)
   obs_norm = matrix(0,nrow = 0,ncol =0)
+  Theta = list()
+  Theta_init = list()
+  kappa_init = sigma_init = xisite_init= rep(NA,nstat)
   for (station in 1:nstat){
+    Sigma_names[station] = paste("sigma",station,sep="_")
+    Kappa_names[station] = paste("kappa",station,sep="_")
+    
     y = M[,station]
     y = y[!is.na(y)]
     y = y[y>thres]-thres#consider only positive precipitation when thres =0
-    as_mean[station] = moy= mean(y,na.rm=TRUE)
+    moy = mean(y,na.rm=TRUE)
     obs_rr = rbind.fill.matrix(obs_rr,t(as.matrix(y)))#and fill the matrix with NA if needed
     obs_norm = rbind.fill.matrix(obs_norm,t(as.matrix(y/moy)))#and fill the matrix with NA if needed
-  }
+  }#end loop on station for data formatting
   M = t(obs_rr)
   MN=t(obs_norm)
-  fitreg = as.data.frame(matrix(NA,nrow = 4,ncol=nstat),
-                         row.names = c("kappa","sigma","xi.site","xi.reg"))
+  #definir Theta et sa forme (matrice ? liste ? dataframe ?)
+  
   for (station in 1:nstat) {
-    y = na.omit(M[,station])
-    as_Lcensor_thres[station] = cens_thres[1]/moy
-    as_Rcensor_thres[station] = cens_thres[2]/moy
-    #y = y/mean(y)#test to fit on normalized data
-    InitPar = c(.9,.5,.1)
-    fitreg[1:3,station] = fit.extgp(y[y>0],method = method,init=InitPar,
-                                    censoring = cens_thres,
-                                    plots = FALSE)$fit$pwm
-  }
-  yn = as.vector(MN)
-  InitPar = c(.9,.5,.1)
-  #Xi.reg.pool = fit.extgp(yn[yn>0],method=method,init = InitPar,
-  #censoring = c(max(as_Lcensor_thres),min(as_Rcensor_thres)),plots = FALSE)$fit$pwm[3]
-  Xi.reg.mean = mean(data.matrix(fitreg[3,]))
-  #if(ParReg=="pool"){xi.reg = Xi.reg.pool}
-  if(ParReg=="mean"){xi.reg = Xi.reg.mean}
-  Kappa.site = as.vector(data.matrix(fitreg[1,]))
-  SigmaSemiReg = as_mean*xi.reg/(Kappa.site*beta(Kappa.site,1-xi.reg)-1)
-  fitreg[2,] = SigmaSemiReg
-  fitreg[4,] = rep(xi.reg,nstat)
-  return(fitreg)
+    y =na.omit(M[,station])
+    fit_init = fit.extgp(y,model=1,method=method,init =ParInit, censoring = cens_thres, rounded = round,plots = FALSE)
+    kappa_init[station] = fit_init$fit$pwm[1]
+    sigma_init[station] = fit_init$fit$pwm[2]
+    xisite_init[station] = fit_init$fit$pwm[3]
+  }#end loop on station for initialization
+  xi_init = mean(xisite_init)
+  Theta_init = list("kappa"=kappa_init,"sigma"=sigma_init,"xi.reg"=rep(xi_init,nstat))
+  
+  Theta = Theta_init
+  Theta_0 = list("kappa"=kappa_init,"sigma"=sigma_init,"xi.site"=xisite_init,"xi.reg"=rep(xi_init,nstat))
+  loop <-0; increment <-precision +.1 
+  log.init<-0
+  Xi.old <- unique(Theta$xi.reg)
+  u = cens_thres[1]
+  while((loop<loop.max)&(increment>precision)){
+    loop <- loop+1
+    Sigma.old <- Theta$sigma
+    Kappa.old <- Theta$kappa
+    
+    
+    for (i in 1:nstat){ y = na.omit(M[,i])
+    CensoredMean[i] <- mean(y[y>u],na.rm=TRUE)
+    }#at-site censored mean 
+    
+    Sigma.new <- (Xi.old*CensoredMean)/(Kappa.old*IB(H(u,Sigma.old,Xi.old),
+                                                     1,Kappa.old,1-Xi.old)/Fbar(u,Kappa.old,
+                                                                                Sigma.old,Xi.old)-1)
+    Theta$sigma <- Sigma.new
+    for (i in 1:nstat){
+      y = na.omit(M[,i])
+      moyN[i] = mean(y[y/Sigma.new[i]>u/Sigma.new[i]]/Sigma.new[i],na.rm=TRUE)}# at site censored mean (data normalized by sigma.new)
+    
+    Kappa.new = (Xi.old*moyN)/(1*IB(H(u,Sigma.new,Xi.old),
+                                    1,Kappa.old,1-Xi.old)/Fbar(u,Kappa.old,
+                                                               Sigma.new,Xi.old)-1/Kappa.old)
+    Theta$kappa = Kappa.new
+    #update kappa and sigma in Theta
+    
+    increment<-max(abs(Sigma.old-Theta$sigma),abs(Kappa.old-Theta$kappa))
+    
+  }#end while
+  
+  
+  return(list("Theta"=Theta,"Theta_0"=Theta_0))
 }
-
-
 
 
 
